d2682a543ebaaa90db4bea98ef56363db75bfa2d
Mio-01 <nikitka.orlov01@bk.ru>
2023-11-29 18:17:31 +0300

    хрень какая-то

--- FacadeForge/FacadeForgeUI/Data/ApplicationDbContext.cs
+++ FacadeForge/FacadeForgeUI/Data/ApplicationDbContext.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using FacadeForgeUI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FacadeForgeUI.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Material> Materials { get; set; }
         public DbSet<Tool> Tools { get; set; }
